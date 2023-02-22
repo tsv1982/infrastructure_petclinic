a6a7df01ed111f5c5fcf08ebae7461ce38d8fbc3
tsv1982 <tsv198250@gmail.com>
2023-02-22 19:20:42 +0000

    sdfs

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
-#output "nat_gateway_ip" {
-#  value = aws_eip.eip_nat_gateway.public_ip
-#}
+output "nat_gateway_ip" {
+  value = aws_eip.eip_nat_gateway.public_ip
+}
 
 output "instance_private_ip" {
   value = aws_instance.mysql_instance.private_ip
